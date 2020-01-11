c3a8f5d13144394fd89e9813f256416b49822eec
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-01-11 17:47:17 +0200

    Removes notice from makeGlobalAppState()
    It was distracting and not entirely true as the function does a sufficient amount of things for the time being.

--- src/contexts/global-app-state/makeGlobalAppState.js
+++ src/contexts/global-app-state/makeGlobalAppState.js
@@ -1,6 +1,4 @@
 function makeGlobalAppState(dehydratedAppState, _app) {
-  // TODO: implement makeGlobalAppState()!
-  console.log("makeGlobalAppState() isn't implemented yet!")
   dehydratedAppState.setCookieConsent = _app.setCookieConsent.bind(_app)
   return dehydratedAppState
 }
