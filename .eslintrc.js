6eec86f4dc2e13487a66d535ff01c6ebd9072cc3
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-01-11 13:48:18 +0200

    Adds more ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,9 @@ module.exports = {
   rules: {
     semi: [1, "never"],
     quotes: [1, "double"],
+    "object-curly-spacing": [1, "never"],
+    "array-bracket-spacing": [1, "never"],
+    "react/jsx-curly-brace-presence": [1, {props: "always", children: "never"}],
     "max-len": 0,
     "require-jsdoc": 0,
     "react/react-in-jsx-scope": 0,
