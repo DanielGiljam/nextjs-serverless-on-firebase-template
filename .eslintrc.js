0e36a0eeab1633e01b3ec1cd3153d5605eda64c0
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-03-18 16:29:30 +0200

    Changes eslintrc.js
    - Installs TypeScript ESLint plugin.
    - Installs import plugin.
    - Adds rule for sorting JSX props.
    - Adds rule for sorting imports.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,13 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "google"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "google",
+  ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
@@ -16,12 +22,58 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ["react", "@typescript-eslint", "import"],
   rules: {
     "array-bracket-spacing": [1, "never"],
+    "import/order": [
+      1,
+      {
+        pathGroups: [
+          {pattern: "react", group: "external", position: "before"},
+          {pattern: "next{,/**}", group: "external", position: "before"},
+          {
+            pattern: "@material-ui/{core,icons,lab}{,/!(styles|colors)*}",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@material-ui/core/styles{,/*}",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@material-ui/core/colors{,/*}",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "color",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "nextjs-global-app-state{,/**}",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["unknown"],
+        "newlines-between": "always",
+        alphabetize: {order: "asc"},
+      },
+    ],
     "max-len": 0,
     "object-curly-spacing": [1, "never"],
     "react/jsx-curly-brace-presence": [1, {props: "always", children: "never"}],
+    "react/jsx-sort-props": [
+      1,
+      {
+        callbacksLast: true,
+        shorthandLast: true,
+        ignoreCase: true,
+        reservedFirst: true,
+      },
+    ],
     "react/react-in-jsx-scope": 0,
     "react/prop-types": 0,
     "require-jsdoc": 0,
