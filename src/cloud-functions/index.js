a8da3396a20d28bad5bc146d97af9e9bb8dd7931
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-03-21 11:34:52 +0200

    Reconfigures Firebase deployment rules

--- src/cloud-functions/index.js
+++ src/cloud-functions/index.js
@@ -3,19 +3,11 @@
 const functions = require("firebase-functions")
 
 const lang = require("../../.next/serverless/pages/[..._lang]")
-const about = require("../../.next/serverless/pages/about")
-const example = require("../../.next/serverless/pages/example/[index]")
 const index = require("../../.next/serverless/pages/index")
 
 module.exports = {
   next: {
     index: functions.https.onRequest(index.render),
-    about: functions.https.onRequest(about.render),
-    example: functions.https.onRequest(example.render),
     lang: functions.https.onRequest(lang.render),
-    serverless: {
-      index: functions.https.onRequest(index.render),
-      about: functions.https.onRequest(about.render),
-    },
   },
 }
