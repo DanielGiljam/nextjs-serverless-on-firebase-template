9186682b2438a73e702522e7fcda7f2b5e31b28d
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-03-21 14:37:43 +0200

    Adds icon array and removes data.json from public directory

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -73,6 +73,67 @@ class _document extends __document {
     return (
       <Html lang={this.props.__NEXT_DATA__.props.dehydratedState.lang.value}>
         <Head>
+          <link href={"/icons/favicon-16.png"} rel={"icon"} sizes={"16x16"} />
+          <link href={"/icons/favicon-32.png"} rel={"icon"} sizes={"32x32"} />
+          <link href={"/icons/favicon-57.png"} rel={"icon"} sizes={"57x57"} />
+          <link href={"/icons/favicon-76.png"} rel={"icon"} sizes={"76x76"} />
+          <link href={"/icons/favicon-96.png"} rel={"icon"} sizes={"96x96"} />
+          <link
+            href={"/icons/favicon-128.png"}
+            rel={"icon"}
+            sizes={"128x128"}
+          />
+          <link
+            href={"/icons/favicon-144.png"}
+            rel={"icon"}
+            sizes={"144x144"}
+          />
+          <link
+            href={"/icons/favicon-152.png"}
+            rel={"icon"}
+            sizes={"152x152"}
+          />
+          <link
+            href={"/icons/favicon-180.png"}
+            rel={"icon"}
+            sizes={"180x180"}
+          />
+          <link
+            href={"/icons/favicon-192.png"}
+            rel={"icon"}
+            sizes={"192x192"}
+          />
+
+          <link
+            href={"/icons/apple-touch-icon-precomposed-180.png"}
+            rel={"apple-touch-icon-precomposed"}
+            sizes={"180x180"}
+          />
+          <link
+            color={"#262626"}
+            href={"/icons/icon-mask.svg"}
+            rel={"mask-icon"}
+          />
+
+          <meta content={"#262626"} name={"msapplication-TileColor"} />
+          <meta
+            content={"/icons/msapplication/mediumtile-144.png"}
+            name={"msapplication-TileImage"}
+          />
+
+          <meta
+            content={"Next.js Serverless on Firebase Demo"}
+            name={"application-name"}
+          />
+          <meta
+            content={"Next.js Serverless on Firebase Demo"}
+            name={"msapplication-tooltip"}
+          />
+          <meta
+            content={"/icons/msapplication/ieconfig.xml"}
+            name={"msapplication-config"}
+          />
+
           <link
             href={
               "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
